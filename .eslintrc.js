031dab4c3f798cfb2ac90879174cf4f30cdda80a
Damian Sznajder <damsznajder@gmail.com>
2019-11-13 17:59:10 +0100

    test: setup jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,26 @@
 module.exports = {
-  extends: "satya164",
+  extends: 'satya164',
   settings: {
     react: {
-      version: "detect"
+      version: 'detect',
     },
-    "import/core-modules": ["rn-calendar"]
+    'import/core-modules': ['rn-calendar'],
+    'import/resolver': {
+      node: {
+        extensions: [
+          '.android.js',
+          '.android.ts',
+          '.android.tsx',
+          '.ios.js',
+          '.ios.ts',
+          '.ios.tsx',
+          '.js',
+          '.native.js',
+          '.ts',
+          '.tsx',
+        ],
+      },
+    },
   },
-  env: { browser: true, node: true }
+  env: { browser: true, node: true },
 };
