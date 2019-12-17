d451b199add6634556c1d837b2d4c51fc69d31cf
Damian Sznajder <damsznajder@gmail.com>
2019-12-17 12:46:16 +0000

    docs: update example with locale changing

--- Example/App.tsx
+++ Example/App.tsx
@@ -1,17 +1,62 @@
 import React, { useState } from 'react';
 import { SafeAreaView, Button, StatusBar } from 'react-native';
 
-import { Calendar } from 'rn-calendar';
+import { Calendar, Locales } from 'rn-calendar';
+
+Locales.en = Locales.default;
+Locales.pl = {
+  monthNames: [
+    'Styczeń',
+    'Luty',
+    'Marzec',
+    'Kwiecień',
+    'Maj',
+    'Czerwiec',
+    'Lipiec',
+    'Sierpień',
+    'Wrzesień',
+    'Październik',
+    'Listopad',
+    'Grudzień',
+  ],
+  monthNamesShort: [
+    'sty',
+    'lut',
+    'mar',
+    'kwi',
+    'maj',
+    'cze',
+    'lip',
+    'sie',
+    'wrz',
+    'paź',
+    'lis',
+    'gru',
+  ],
+  dayNames: [
+    'Niedziela',
+    'Poniedziałek',
+    'Wtorek',
+    'Środa',
+    'Czwartek',
+    'Piątek',
+    'Sobota',
+  ],
+  dayNamesShort: ['ndz', 'pon', 'wt', 'śr', 'czw', 'pt', 'sob'],
+};
 
 const App = () => {
   const [firstDay, setFirstDay] = useState<0 | 1>(0);
+  const [locale, setLocale] = useState<'en' | 'pl'>('en');
 
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
+      <SafeAreaView
+        style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+      >
         <Calendar
-          calendarHeight={380}
+          locale={locale}
           firstDay={firstDay}
           startISODate="2014-01-01"
           endISODate="2019-12-31"
@@ -23,7 +68,7 @@ const App = () => {
         />
         <Button
           title="Change Locale"
-          onPress={() => setFirstDay(firstDay => (firstDay ? 0 : 1))}
+          onPress={() => setLocale(locale => (locale === 'en' ? 'pl' : 'en'))}
         />
       </SafeAreaView>
     </>
