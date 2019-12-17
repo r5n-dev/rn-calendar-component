0d949bf418f969dd13efe644a51d16fbf062ba36
Damian Sznajder <damsznajder@gmail.com>
2019-12-17 15:50:03 +0000

    chore: export types for components

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,10 @@
 export * from './Calendar';
 
 export { default as Locales } from './Locales';
+
+export {
+  DayComponentProps,
+  DayNamesComponentProps,
+  MonthTitleComponentProps,
+  Locale,
+} from './types';
