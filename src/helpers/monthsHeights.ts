3b8ba51b03132cf70149e9c55466778e340e59dd
Damian Sznajder <damsznajder@gmail.com>
2020-05-15 10:45:18 +0200

    perf: optimize offset

--- src/helpers/monthsHeights.ts
+++ src/helpers/monthsHeights.ts
@@ -1,25 +1,28 @@
 import { CalendarDate } from '../types';
 import constants from './constants';
 
-let cachedMonths: Array<{ height: number }> = [];
+let cachedMonths: Array<{ height: number; offset: number }> = [];
 
 const monthsHeights = (monthsData: Array<[string, Array<CalendarDate>]>) => {
   if (monthsData.length === cachedMonths.length) {
     return cachedMonths;
   }
 
-  const data = monthsData.map(([_month, data]) => {
+  const data = monthsData.reduce((acc, [_month, data]) => {
     const firstDay = data[0];
     const firstWeekDaysCount = constants.weekLength - firstDay.dayOfWeek;
     const restWeeksCount = data.slice(firstWeekDaysCount).length / 7;
     const lastWeekDaysCount = restWeeksCount % 7;
     const weeksCount =
       1 + Math.floor(restWeeksCount) + Number(lastWeekDaysCount > 0);
-    return {
-      height:
-        weeksCount * constants.touchableSize + constants.monthHeaderHeight,
-    };
-  });
+    const height =
+      weeksCount * constants.touchableSize + constants.monthHeaderHeight;
+    const offset = acc.reduce((acc, { height }) => (acc += height), 0);
+    acc.push({ height, offset });
+
+    return acc;
+  }, [] as Array<{ height: number; offset: number }>);
+
   cachedMonths = data;
   return data;
 };
